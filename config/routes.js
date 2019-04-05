580278ad5fa0cef45581564b94c65c9bc0018215
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-05 12:09:46 -0400

    Implement login function

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@ const axios = require("axios")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const db = require("../database/dbConfig")
+const secret = process.env.JWT_SECRET
 
 const { authenticate } = require("../auth/authenticate")
 
@@ -24,8 +25,12 @@ function register(req, res) {
 
   db("users")
     .insert({ username, password: hash })
-    .then(saved => {
-      res.status(201).json(saved)
+    .then(([id]) => {
+      res.status(201).json({
+        username,
+        password: hash,
+        id
+      })
     })
     .catch(error => {
       res.status(500).json(error)
@@ -33,10 +38,34 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    res.status(400).json({
+      message: "Please provide a username and password."
+    })
+  }
+
+  db("users")
+    .where({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user)
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 }
 
-function getJokes(req, res) {
+function getJokes(_req, res) {
   const requestOptions = {
     headers: { accept: "application/json" }
   }
@@ -49,4 +78,15 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: "Error Fetching Jokes", error: err })
     })
+}
+
+function genToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: "1d"
+  }
+  return jwt.sign(payload, secret, options)
 }
